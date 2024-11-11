b6a3c20e029ced2633ef74e05b4800eec109e7e3
iamnippon <mindbend91@gmail.com>
2024-11-11 22:14:39 +0600

    update A.py

--- ICPC Rookie Sheet/Sheet #1 Data Type/#A Hellon With Python/A.py
+++ ICPC Rookie Sheet/Sheet #1 Data Type/#A Hellon With Python/A.py
@@ -1,2 +1,7 @@
-name = input()
-print("Hello, "+name)
+
+#Question A
+#Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/A
+
+#Solution:
+name = input() #Take the name from the user
+print("Hello, "+name) #Print the name with Hello, in the beginning
