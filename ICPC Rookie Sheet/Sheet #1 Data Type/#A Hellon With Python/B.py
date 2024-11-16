31caa515addec814ad88439ccb8aa5e5de16e2d4
iamnippon <mindbend91@gmail.com>
2024-11-16 13:05:19 +0600

    Your commit message here

---
+++ ICPC Rookie Sheet/Sheet #1 Data Type/#A Hellon With Python/B.py
@@ -0,0 +1,11 @@
+#Question B
+#Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/B
+
+#Solution:
+data = input().split() #Take the inpute and split it
+
+print(int(data[0]))
+print(int(data[1]))
+print((data[2]))
+print(float(data[3]))
+print(float(data[4]))
