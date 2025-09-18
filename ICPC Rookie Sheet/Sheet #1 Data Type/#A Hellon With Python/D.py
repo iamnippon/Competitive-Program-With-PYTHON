df5f8e1dbacd0c4448f11faccc964c4668c93ecd
iamnippon <mindbend91@gmail.com>
2025-09-19 00:54:31 +0600

    solve D and E

---
+++ ICPC Rookie Sheet/Sheet #1 Data Type/#A Hellon With Python/D.py
@@ -0,0 +1,7 @@
+#Question D
+#Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/D
+
+#Solution:
+a, b, c, d = map(int, input().split())
+
+print(f"Difference = {a*b - c*d}")
---
+++ ICPC Rookie Sheet/Sheet #1 Data Type/#A Hellon With Python/E.py
@@ -0,0 +1,6 @@
+#Question E
+#Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/E
+
+#Solution:
+a = float(input()) #Take the radius of the circle from the user
+print(a*a*3.141592653) #Area of Circle = π*r^2 where r is the radius of the circle
